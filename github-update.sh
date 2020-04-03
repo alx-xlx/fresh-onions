55ca3b94d5c200cc456c16870e4e0a033c343baf
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-04-04 04:49:15 +0530

    github-update: create

---
+++ github-update.sh
@@ -0,0 +1,7 @@
+git pull https://github.com/alx-xlx/fresh-onions.git
+git config user.name "From Server"
+git config user.email "alexsxd7@gmail.com"
+git add .
+git commit -m "From-remote-server"
+git config credential.helper store
+git push https://github.com/alx-xlx/fresh-onions.git
